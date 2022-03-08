e7432bdeb4ac6589504d9c0cbde8da35bebfe8be
Jose Antonio G <jogw@gft.com>
2022-03-08 18:16:05 +0000

    new-bicket

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "google_storage_bucket" "bucket" {
   name = "test-bucket-random-0011221"
   location = "europe-west1"
 }
+
+resource "google_storage_bucket" "bucket" {
+  name = "test-bucket-random-0011231"
+  location = "europe-west1"
+}
