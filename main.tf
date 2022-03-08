3bcb0b628ba2b93935509ca176f8e24149b8ad71
Jose Antonio G <jogw@gft.com>
2022-03-08 18:24:30 +0000

    bucket in different zone

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ resource "google_storage_bucket" "bucket" {
 
 resource "google_storage_bucket" "bucket" {
   name = "test-bucket-random-0011231"
-  location = "europe-west1"
+  location = "europe-west2"
 }
